96e431041b9f411ef0c1d37b4acda23da2ff8394
Abdukarimov Ziyodulla <ZiyodullaDeveloper@gmail.com>
2022-07-17 06:41:02 +0000

    model qo'shildi

--- abstractuser/models.py
+++ abstractuser/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    HOD = '1'
+    STAFF = '2'
+    STUDENT = '3'
+
+
+    EMAIL_TO_USER_TYPE_MAP = {
+        'hod': HOD,
+        'staff': STAFF,
+        'student': STUDENT
+    }
+
+    user_type_data = ((HOD, "HOD"), (STAFF, "Staff"), (STUDENT, "Client"))
+    user_type = models.CharField(
+        default=1, choices=user_type_data, max_length=10)
+
+
+class AdminHod(models.Model):
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='admin')
+    id = models.AutoField(primary_key=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+
+class Staffs(models.Model):
+    id = models.AutoField(primary_key=True)
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='staff')
+    address = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+class Client(models.Model):
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='client')
+    id = models.AutoField(primary_key=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+
+    def __str__(self):
+        return self.admin.username
